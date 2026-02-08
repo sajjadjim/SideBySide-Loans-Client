11c8006dba7e2645492be2e2a28f159552ed1e60
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2026-02-09 00:38:48 +0600

    Firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,30 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_projectId,
-//   storageBucket:  import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD7YNQ68fV9BPTMlEp7V0PGzTEnSLzviA0",
-  authDomain: "sidebyside-loans.firebaseapp.com",
-  projectId: "sidebyside-loans",
-  storageBucket: "sidebyside-loans.firebasestorage.app",
-  messagingSenderId: "229338225511",
-  appId: "1:229338225511:web:f9f16cdfed2f5d535cedc2"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket:  import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
-
 export default app;
 
